d9cb7e1c39c177e269e7f0d33987f7c3e14bd689
nir102030 <nir102030@gmail.com>
2021-03-01 01:23:00 +0200

    Updates

---
+++ precache-manifest.63dad517486c86a3f011085c6a395c1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d00dd2b16c1d3b75e1f5",
+    "url": "/me/static/css/main.0d8c1ff6.chunk.css"
+  },
+  {
+    "revision": "d00dd2b16c1d3b75e1f5",
+    "url": "/me/static/js/main.d00dd2b1.chunk.js"
+  },
+  {
+    "revision": "66be97a19e235fa95a6c",
+    "url": "/me/static/js/runtime~main.66be97a1.js"
+  },
+  {
+    "revision": "77e2a8e6f10b7a79c9ca",
+    "url": "/me/static/js/2.77e2a8e6.chunk.js"
+  },
+  {
+    "revision": "ac0a66af933de27ac1a4fcc3ccb8969f",
+    "url": "/me/static/media/resume.ac0a66af.pdf"
+  },
+  {
+    "revision": "99ae631db08be6eee47e9a51cd968d27",
+    "url": "/me/index.html"
+  }
+];
