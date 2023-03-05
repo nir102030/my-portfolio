75990fa1aa8cf15bb73c6eb4c6cd086c80076611
Nir Katz <nir.katz@kaltura.com>
2023-03-05 21:12:43 +0200

    update resume, styling and content

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,96 +1,99 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class About extends Component {
-	render() {
-		return (
-			<div>
-				<section className="colorlib-about" data-section="about">
-					<div className="colorlib-narrow-content">
-						<div className="row">
-							<div className="col-md-12">
-								<div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
-									<div className="col-md-12">
-										<div className="about-desc">
-											<span className="heading-meta">About Me</span>
-											<h2 className="colorlib-heading">Who Am I?</h2>
-											<p>
-												I am a Fullstack developer. I studied Industrial Engeineering at
-												Ben-Gurion University majoring in Information Systems. Soon, I realized
-												my future is in coding, and started to explore the programming world,
-												particularly Mobile and Web development. I've started to build my own
-												projects, and was a part of a BGU Hackathon winner project, developing a
-												smart system for exam checking using react and node.js.
-											</p>
-											<p>
-												In addition, I work as an application engineer at Quality Forward, and I
-												recently initiated a small business for web and mobile development,
-												currently making its first steps.
-											</p>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</section>
-				<section className="colorlib-about">
-					<div className="colorlib-narrow-content">
-						<div className="row">
-							<div
-								className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
-								data-animate-effect="fadeInLeft"
-							>
-								<span className="heading-meta">What I do?</span>
-								<h2 className="colorlib-heading">Here are some of my expertise</h2>
-							</div>
-						</div>
-						<div className="row row-pt-md">
-							<div className="col-md-4 text-center animate-box">
-								<div className="services color-1">
-									<span className="icon">
-										<i className="icon-bulb" />
-									</span>
-									<div className="desc">
-										<h3>Web Development </h3>
-										<p>
-											I have experience building web applications using JavaScript,React,HTML,CSS
-										</p>
-									</div>
-								</div>
-							</div>
-							<div className="col-md-4 text-center animate-box">
-								<div className="services color-3">
-									<span className="icon">
-										<i className="icon-phone3" />
-									</span>
-									<div className="desc">
-										<h3>Mobile Development</h3>
-										<p>
-											I have experience bulding mobile applications using React-Native for the
-											front-end and node.js for the backend
-										</p>
-									</div>
-								</div>
-							</div>
-							<div className="col-md-4 text-center animate-box">
-								<div className="services color-5">
-									<span className="icon">
-										<i className="icon-data" />
-									</span>
-									<div className="desc">
-										<h3>Databases</h3>
-										<p>
-											Coming from the IE background, I have a good grasp over fundamental concepts
-											of DB. I have experience using SQL and no-SQL framworks like SQL-Server and
-											Mongo
-										</p>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</section>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <section className="colorlib-about" data-section="about">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-12">
+                <div
+                  className="row row-bottom-padded-sm animate-box"
+                  data-animate-effect="fadeInLeft"
+                >
+                  <div className="col-md-12">
+                    <div className="about-desc">
+                      <span className="heading-meta">About Me</span>
+                      <h2 className="colorlib-heading">Who Am I?</h2>
+                      <p>
+                        I am a Fullstack developer. I studied Industrial
+                        Engineering at Ben-Gurion University majoring in
+                        Information Systems. Soon, I realized my future is in
+                        coding, and started to explore the programming world,
+                        particularly Mobile and Web development. These days, I
+                        work as a frontend engineer at Kaltura, develop next
+                        generation Cloud TV app, used by more then million
+                        households worldwide.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="colorlib-about">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div
+                className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
+                data-animate-effect="fadeInLeft"
+              >
+                <span className="heading-meta">What I do?</span>
+                <h2 className="colorlib-heading">
+                  Here are some of my expertise
+                </h2>
+              </div>
+            </div>
+            <div className="row row-pt-md">
+              <div className="col-md-4 text-center animate-box">
+                <div className="services color-1">
+                  <span className="icon">
+                    <i className="icon-bulb" />
+                  </span>
+                  <div className="desc">
+                    <h3>Mobile Development</h3>
+                    <p>
+                      I have experience bulding mobile applications using
+                      React-Native for the front-end and node.js for the backend
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 text-center animate-box">
+                <div className="services color-3">
+                  <span className="icon">
+                    <i className="icon-phone3" />
+                  </span>
+                  <div className="desc">
+                    <h3>Web Development </h3>
+                    <p>
+                      I have experience building web applications using
+                      JavaScript,React,HTML,CSS
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 text-center animate-box">
+                <div className="services color-5">
+                  <span className="icon">
+                    <i className="icon-data" />
+                  </span>
+                  <div className="desc">
+                    <h3>Databases</h3>
+                    <p>
+                      I have a good grasp over fundamental concepts of DB. I
+                      have experience using SQL and no-SQL frameworks like
+                      SQL-Server and MongoDB.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -1,93 +1,137 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Timeline extends Component {
-	render() {
-		return (
-			<div>
-				<section className="colorlib-experience" data-section="timeline">
-					<div className="colorlib-narrow-content">
-						<div className="row">
-							<div
-								className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
-								data-animate-effect="fadeInLeft"
-							>
-								<span className="heading-meta">highlights</span>
-								<h2 className="colorlib-heading animate-box">Timeline</h2>
-							</div>
-						</div>
-						<div className="row">
-							<div className="col-md-12">
-								<div className="timeline-centered">
-									<article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
-										<div className="timeline-entry-inner">
-											<div className="timeline-icon color-3">
-												<i className="icon-pen2" />
-											</div>
-											<div className="timeline-label">
-												<h2>
-													Application Engineer at Quality Forward <span>2019-present</span>
-												</h2>
-												<p>
-													Quality Forward is an early stages startup in the E-QMS field. My
-													responsibilites are developing new features, taking care of costumer
-													requirements, performing integrations etc. Some of my work involve
-													coding and some involve communication with the customers in order to
-													get wider understanding of their needs and to supply the best result
-													in the system. In my work I'm using JS, Ajax, Jquery, HTML, Python,
-													HTTP protocol and more.
-												</p>
-											</div>
-										</div>
-									</article>
-									<article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
-										<div className="timeline-entry-inner">
-											<div className="timeline-icon color-4">
-												<i className="icon-pen2" />
-											</div>
-											<div className="timeline-label">
-												<h2>
-													Mobile and Web development Freelance<span></span>
-												</h2>
-												<p>
-													While working on several projects I'm currently making my first
-													steps as a mobile and web freelance
-												</p>
-											</div>
-										</div>
-									</article>
-									<article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
-										<div className="timeline-entry-inner">
-											<div className="timeline-icon color-5">
-												<i className="icon-pen2" />
-											</div>
-											<div className="timeline-label">
-												<h2>
-													Undergraduate at Ben-Gurion University <span>2016-2020</span>
-												</h2>
-												<p>
-													I did my bachelor's degree in Industrial Engineering majoring in
-													Information Systems. I have taken courses like OOP in JAVA, DBs,
-													Algorithms, Machine Learning etc. and have better understanding of
-													these subjects. I have also took some external courses on mobile and
-													web development.
-												</p>
-											</div>
-										</div>
-									</article>
-									<article
-										className="timeline-entry begin animate-box"
-										data-animate-effect="fadeInBottom"
-									>
-										<div className="timeline-entry-inner">
-											<div className="timeline-icon color-none"></div>
-										</div>
-									</article>
-								</div>
-							</div>
-						</div>
-					</div>
-				</section>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div
+                className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
+                data-animate-effect="fadeInLeft"
+              >
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInLeft"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>
+                          Frontend Developer at Kaltura{" "}
+                          <span>2021-present</span>
+                        </h2>
+                        <p>
+                          Kaltura is an Israeli company specialized on
+                          delivering high quality video based products, like
+                          virtual events platform or a virtual classroom
+                          platform. The product I'm working on is a Cloud TV
+                          app, that allows media and telecom companies release
+                          an end-to-end TV app to their users in less then a few
+                          months. I'm working on the client side of the app,
+                          using React Native and TypeScript, and was part of the
+                          first coding lines of this app. The app is now in
+                          production, with more then million households
+                          worldwide.
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInTop"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>
+                          Mobile and Web development Freelance{" "}
+                          <span>2020-2021</span>
+                        </h2>
+                        <p>
+                          While working on several projects I'm currently making
+                          my first steps as a mobile and web freelance
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInLeft"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>
+                          Application Engineer at Quality Forward{" "}
+                          <span>2019-2021</span>
+                        </h2>
+                        <p>
+                          Quality Forward is an early stages startup in the
+                          E-QMS field. My responsibilites are developing new
+                          features, taking care of costumer requirements,
+                          performing integrations etc. Some of my work involve
+                          coding and some involve communication with the
+                          customers in order to get wider understanding of their
+                          needs and to supply the best result in the system. In
+                          my work I'm using JS, Ajax, Jquery, HTML, Python, HTTP
+                          protocol and more.
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInLeft"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>
+                          Undergraduate at Ben-Gurion University{" "}
+                          <span>2016-2020</span>
+                        </h2>
+                        <p>
+                          I did my bachelor's degree in Industrial Engineering
+                          majoring in Information Systems. I have taken courses
+                          like OOP in JAVA, DBs, Algorithms, Machine Learning
+                          etc. and have better understanding of these subjects.
+                          I have also took some external courses on mobile and
+                          web development.
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article
+                    className="timeline-entry begin animate-box"
+                    data-animate-effect="fadeInBottom"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none"></div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
